f57e73edbbeb23926c08018b617e3904f671d9ed
timofey.tsushko <dared3vil77@gmail.com>
2016-04-14 23:52:40 +0300

    initial gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
+var gulp 	= require('gulp'),
+	del     = require('del'),
+    rename  = require('gulp-rename'),
+    sass    = require('gulp-sass');
 
-gulp.task('default', function() {
-  console.log("Hey");
+var config  = require('./config.json');
+config.paths.distCss = config.paths.dest + '/css'
+
+gulp.task('clean', function (cb) {
+    del.sync([config.paths.dest], cb);   
 });
+
+gulp.task('html', function () {
+    return gulp.src(['src/*.html'])
+           .pipe(gulp.dest(config.paths.dest));    
+});
+
+gulp.task('styles', function () {
+    return gulp.src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(rename('app.css'))
+        .pipe(gulp.dest(config.paths.distCss))
+});
+
+gulp.task('dev', ['clean', 'html', 'styles']);
